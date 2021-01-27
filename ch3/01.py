4be851394a699a7831147d007354a83047a4c583
dly <1083404373@qq.com>
2021-01-27 20:58:31 +0800

    调色板

--- ch3/01.py
+++ ch3/01.py
@@ -55,4 +55,41 @@ def tick_count():
     time = (e2 - e1) / cv.getTickFrequency()
 
 
-tick_count()
+# tick_count()
+
+def nothing(x):
+    pass
+
+
+# 滑动条，调色板
+def trackBar():
+    img = np.zeros((512, 512, 3), np.uint8)
+    cv.namedWindow('image')
+
+    cv.createTrackbar('R', 'image', 0, 255, nothing)
+    cv.createTrackbar('G', 'image', 0, 255, nothing)
+    cv.createTrackbar('B', 'image', 0, 255, nothing)
+
+    switch = '0:OFF\n1:ON'
+    cv.createTrackbar(switch, 'image', 0, 1, nothing)
+
+    while 1:
+        cv.imshow('image', img)
+        k = cv.waitKey(1) & 0xFF
+        if k == 27:
+            break
+
+        r = cv.getTrackbarPos('R', 'image')
+        g = cv.getTrackbarPos('G', 'image')
+        b = cv.getTrackbarPos('B', 'image')
+        s = cv.getTrackbarPos(switch, 'image')
+
+        if s == 0:
+            img[:] = 0
+        else:
+            img[:] = [b, g, r]
+
+    cv.destroyAllWindows()
+
+
+trackBar()
